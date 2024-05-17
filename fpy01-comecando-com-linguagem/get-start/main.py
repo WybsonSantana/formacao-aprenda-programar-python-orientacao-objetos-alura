1b49c2d0e60372400f1bac260db372c12e68e7f5
WybsonSantana <wybson@hotmail.com>
2024-05-17 00:47:13 -0300

    fpy01 - aula 1: Função print e variáveis

---
+++ fpy01-comecando-com-linguagem/get-start/main.py
@@ -0,0 +1,10 @@
+print("Olá, mundo!")
+
+print("Brasil", "ganhou", 5, "títulos", "mundiais", sep="-")
+
+pais = "Itália"
+quantidade_de_titutlos = 4
+print(pais, "ganhou", quantidade_de_titutlos, "títulos", "mundiais")
+
+print(type(pais))
+print(type(quantidade_de_titutlos))
