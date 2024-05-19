ffbd57e2008417f0b7ede1730270edf863d95dfa
WybsonSantana <wybson@hotmail.com>
2024-05-19 01:07:19 -0300

    fpy01 - aula 7: Definindo uma pontuação para o usuário

--- fpy01-comecando-com-linguagem/jogos/adivinhacao.py
+++ fpy01-comecando-com-linguagem/jogos/adivinhacao.py
@@ -5,6 +5,7 @@ print("* Bem-vindo ao Jogo de Adivinhação *")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
+total_de_pontos = 1000
 total_de_tentativas = 0
 
 print("Qual o nível de dificuldade?")
@@ -36,12 +37,14 @@ for rodada in range(1, total_de_tentativas + 1):
     chute_menor_que_numero_secreto = chute < numero_secreto
 
     if acertou_chute:
-        print("Você acertou!")
+        print("Você acertou! Você fez {} pontos.".format(total_de_pontos))
         break
     else:
         if chute_maior_que_numero_secreto:
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif chute_menor_que_numero_secreto:
             print("Você errou! O seu chute foi menor que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        total_de_pontos = total_de_pontos - pontos_perdidos
 
 print("Fim do jogo!")
