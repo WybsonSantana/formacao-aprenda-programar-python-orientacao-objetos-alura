e09f423e3434634c9440b9aaf38f86db1153a8f4
WybsonSantana <wybson@hotmail.com>
2024-05-18 23:45:46 -0300

    fpy01 - aula 5: Encerrando a interação e o loop

--- fpy01-comecando-com-linguagem/jogos/adivinhacao.py
+++ fpy01-comecando-com-linguagem/jogos/adivinhacao.py
@@ -8,10 +8,14 @@ total_de_tentativas = 3
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
 
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
 
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou_chute = numero_secreto == chute
     chute_maior_que_numero_secreto = chute > numero_secreto
@@ -19,6 +23,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou_chute:
         print("Você acertou!")
+        break
     else:
         if chute_maior_que_numero_secreto:
             print("Você errou! O seu chute foi maior que o número secreto.")
