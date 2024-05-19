58c6c175488e48e9498b671011a68e11f3eb162e
WybsonSantana <wybson@hotmail.com>
2024-05-19 00:49:06 -0300

    fpy01 - aula 7: Adicionando níveis ao jogo

--- fpy01-comecando-com-linguagem/jogos/adivinhacao.py
+++ fpy01-comecando-com-linguagem/jogos/adivinhacao.py
@@ -5,7 +5,19 @@ print("* Bem-vindo ao Jogo de Adivinhação *")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel_de_dificuldade = int(input("Defina o nível: "))
+
+if nivel_de_dificuldade == 1:
+    total_de_tentativas = 20
+elif nivel_de_dificuldade == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
