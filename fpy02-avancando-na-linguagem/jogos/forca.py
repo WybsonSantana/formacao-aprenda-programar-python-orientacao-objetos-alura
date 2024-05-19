bff44ec3f662b479683650c81e22d86d08032cf0
WybsonSantana <wybson@hotmail.com>
2024-05-19 16:55:49 -0300

    fpy02 - aula 1: Game Loop

--- fpy02-avancando-na-linguagem/jogos/forca.py
+++ fpy02-avancando-na-linguagem/jogos/forca.py
@@ -2,6 +2,15 @@ def jogar():
     print("*********************************")
     print("* Bem-vindo ao Jogo da Forca *")
     print("*********************************")
+
+    palavra_secreta = "banana"
+
+    acertou_palavra = False
+    enforcou = False
+
+    while not acertou_palavra and not enforcou:
+        print("Jogando!")
+
     print("Fim do jogo!")
 
 
