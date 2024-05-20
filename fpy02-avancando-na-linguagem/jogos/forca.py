574c421e56b3fecab7ff7d72f26a551c9d4286ca
WybsonSantana <wybson@hotmail.com>
2024-05-19 21:45:45 -0300

    fpy02 - aula 5: Estipulando tentativas de erros

--- fpy02-avancando-na-linguagem/jogos/forca.py
+++ fpy02-avancando-na-linguagem/jogos/forca.py
@@ -3,22 +3,36 @@ def jogar():
     print("* Bem-vindo ao Jogo da Forca *")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     acertou_palavra = False
     enforcou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while not acertou_palavra and not enforcou:
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou_palavra = "_" not in letras_acertadas
         print(letras_acertadas)
+
+    if acertou_palavra:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo!")
 
