1095b78846aae6bbc3b9e39a564347755eda5543
WybsonSantana <wybson@hotmail.com>
2024-05-22 23:26:44 -0300

    fpy03 - aula 2: Construtor

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/conta.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/conta.py
@@ -1,2 +1,6 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -1,4 +1,7 @@
 from conta import Conta
 
-conta = Conta()
-print(conta)
+conta = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
+print(conta.numero)
+print(conta.titular)
+print(conta.saldo)
+print(conta.limite)
