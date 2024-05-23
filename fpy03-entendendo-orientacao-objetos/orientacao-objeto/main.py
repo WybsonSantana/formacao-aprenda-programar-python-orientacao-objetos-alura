29ea2382ea933097546d5cdef9a2ef13ced6a27f
WybsonSantana <wybson@hotmail.com>
2024-05-22 22:55:26 -0300

    fpy03 - aula 2: Classe e Objeto

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -1,8 +1,4 @@
-import teste
-from teste import depositar, sacar, extrato
+from conta import Conta
 
-conta = teste.criar_conta("1001", "Fulano de Tal", 100.0, 1000.0)
-
-depositar(conta, 300.0)
-sacar(conta, 150.0)
-extrato(conta)
+conta = Conta()
+print(conta)
