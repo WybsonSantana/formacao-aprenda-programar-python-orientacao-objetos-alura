46cdaa52480063f689241ff0ae1f64b41f9a74b6
WybsonSantana <wybson@hotmail.com>
2024-05-24 20:40:45 -0300

    fpy03 - aula 2: Acessando atributos

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -1,7 +1,13 @@
 from conta import Conta
 
-conta = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
-print(conta.numero)
-print(conta.titular)
-print(conta.saldo)
-print(conta.limite)
+conta1 = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
+print(conta1.numero)
+print(conta1.titular)
+print(conta1.saldo)
+print(conta1.limite)
+
+conta2 = Conta("1002", "Bel Trano", 200.0, 1000.0)
+print(conta2.numero)
+print(conta2.titular)
+print(conta2.saldo)
+print(conta2.limite)
