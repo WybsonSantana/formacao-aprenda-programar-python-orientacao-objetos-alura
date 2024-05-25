f33974a97119da79ddbdd09b7653d5d9403cd66c
WybsonSantana <wybson@hotmail.com>
2024-05-24 21:17:50 -0300

    fpy03 - aula 3: Usando métodos

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/conta.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/conta.py
@@ -4,3 +4,14 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def consultar_extrato(self):
+        print("Saldo de R$ {:.2f} do titular {}.".format(self.saldo, self.titular))
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print("Depósito de R$ {:.2f} realizado com sucesso!".format(valor))
+
+    def sacar(self, valor):
+        self.saldo -= valor
+        print("Saque de R$ {:.2f} realizado com sucesso!".format(valor))
--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -1,13 +1,8 @@
 from conta import Conta
 
-conta1 = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
-print(conta1.numero)
-print(conta1.titular)
-print(conta1.saldo)
-print(conta1.limite)
-
-conta2 = Conta("1002", "Bel Trano", 200.0, 1000.0)
-print(conta2.numero)
-print(conta2.titular)
-print(conta2.saldo)
-print(conta2.limite)
+conta = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
+conta.consultar_extrato()
+conta.depositar(200.0)
+conta.consultar_extrato()
+conta.sacar(50.0)
+conta.consultar_extrato()
