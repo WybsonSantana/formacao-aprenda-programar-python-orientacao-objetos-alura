4486a2e1632424321319943fbabc80f6445a6748
WybsonSantana <wybson@hotmail.com>
2024-05-25 14:54:21 -0300

    fpy03 - aula 3: Desafio Opcional

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -1,4 +1,5 @@
 from conta import Conta
+from datas import Data
 
 conta = Conta("1001", "Fulano de Tal", 100.0, 1000.0)
 conta.consultar_extrato()
@@ -6,3 +7,6 @@ conta.depositar(200.0)
 conta.consultar_extrato()
 conta.sacar(50.0)
 conta.consultar_extrato()
+
+data = Data(1, 11, 1920)
+data.formatada()
