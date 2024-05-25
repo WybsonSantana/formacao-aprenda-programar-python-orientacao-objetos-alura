72f9a61491d87673f74003f30fb64d86cc057e9c
WybsonSantana <wybson@hotmail.com>
2024-05-25 18:04:39 -0300

    fpy03 - aula 5: Getters e Setters

--- fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
+++ fpy03-entendendo-orientacao-objetos/orientacao-objeto/main.py
@@ -11,5 +11,8 @@ conta1.transferir(50.0, conta2)
 conta1.consultar_extrato()
 conta2.consultar_extrato()
 
+conta1.set_limite(2000.0)
+print("Limite da conta de {}: R$ {:.2f}".format(conta1.get_titular(), conta1.get_limite()))
+
 data = Data(1, 11, 1920)
 data.formatada()
