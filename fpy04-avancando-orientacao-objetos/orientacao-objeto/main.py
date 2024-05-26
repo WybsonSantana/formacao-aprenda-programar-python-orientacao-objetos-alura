351c88051aeddea8ccda1234f0347ecd3ca59e3f
WybsonSantana <wybson@hotmail.com>
2024-05-26 00:07:27 -0300

    fpy04 - aula 1: Adicionando atributos e métodos

--- fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
@@ -1,7 +1,9 @@
 from modulo import Filme, Serie
 
 filme = Filme("Vingadores - Guerra Infinita", 2018, 160)
-print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}")
+filme.like()
+print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}, likes: {filme.likes}")
 
 serie = Serie("Atlanta", 2018, 2)
-print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie.temporadas}")
+serie.like()
+print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie.temporadas}, likes: {serie.likes}")
