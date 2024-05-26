5d52b124e74340f759edde968756e0d4dfdeb42c
WybsonSantana <wybson@hotmail.com>
2024-05-26 00:55:48 -0300

    fpy04 - aula 1: Encapsulando comportamento

--- fpy04-avancando-orientacao-objetos/orientacao-objeto/modulo.py
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/modulo.py
@@ -1,22 +1,78 @@
 class Filme:
 
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
-        self.duracao = duracao
-        self.likes = 0
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__duracao = duracao
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, novo_ano):
+        self.__ano = novo_ano
+
+    @property
+    def duracao(self):
+        return self.__duracao
+
+    @duracao.setter
+    def duracao(self, nova_duracao):
+        self.__duracao = nova_duracao
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def like(self):
-        self.likes += 1
+        self.__likes += 1
 
 
 class Serie:
 
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.likes = 0
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, novo_ano):
+        self.__ano = novo_ano
+
+    @property
+    def temporadas(self):
+        return self.__temporadas
+
+    @temporadas.setter
+    def temporadas(self, nova_temporada):
+        self.__temporadas = nova_temporada
+
+    @property
+    def likes(self):
+        return self.__likes
 
     def like(self):
-        self.likes += 1
+        self.__likes += 1
