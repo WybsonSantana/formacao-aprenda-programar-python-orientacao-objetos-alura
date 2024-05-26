b76e44def48c6e487c72b995fb0b90cfc12c357a
WybsonSantana <wybson@hotmail.com>
2024-05-25 23:53:07 -0300

    fpy04 - aula 1: Relembrando classes e objetos

---
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
@@ -0,0 +1,7 @@
+from modulo import Filme, Serie
+
+filme = Filme("Vingadores - Guerra Infinita", 2018, 160)
+print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}")
+
+serie = Serie("Atlanta", 2018, 2)
+print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie.temporadas}")
