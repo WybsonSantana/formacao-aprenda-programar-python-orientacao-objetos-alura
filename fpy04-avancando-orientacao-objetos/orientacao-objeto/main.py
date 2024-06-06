1b27c346c6630e7e3db0db9db39632359aaa8c0f
WybsonSantana <wybson@hotmail.com>
2024-06-05 23:36:51 -0300

    fpy04 - aula 2: Explicando a herança

--- fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
@@ -2,8 +2,8 @@ from modulo import Filme, Serie
 
 filme = Filme("Vingadores - Guerra Infinita", 2018, 160)
 filme.like()
-print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme._duracao}, likes: {filme.likes}")
+print("{filme.nome} - {filme.duracao}: {filme.likes}")
 
 serie = Serie("Atlanta", 2018, 2)
 serie.like()
-print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie._temporadas}, likes: {serie._likes}")
+print("{serie.nome} - {serie.temporadas}: {serie.likes}")
