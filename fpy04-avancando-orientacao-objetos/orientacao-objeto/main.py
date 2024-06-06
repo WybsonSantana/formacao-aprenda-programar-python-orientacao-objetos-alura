b1e224c2fa67240550c65e03fba97b493b6f4500
WybsonSantana <wybson@hotmail.com>
2024-06-05 23:27:31 -0300

    fpy04 - aula 2: Removendo duplicação

--- fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/main.py
@@ -2,8 +2,8 @@ from modulo import Filme, Serie
 
 filme = Filme("Vingadores - Guerra Infinita", 2018, 160)
 filme.like()
-print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme.duracao}, likes: {filme.likes}")
+print(f"Nome: {filme.nome}, Ano: {filme.ano}, Duração: {filme._duracao}, likes: {filme.likes}")
 
 serie = Serie("Atlanta", 2018, 2)
 serie.like()
-print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie.temporadas}, likes: {serie.likes}")
+print(f"Nome: {serie.nome}, Ano: {serie.ano}, Temporadas: {serie._temporadas}, likes: {serie._likes}")
--- fpy04-avancando-orientacao-objetos/orientacao-objeto/modulo.py
+++ fpy04-avancando-orientacao-objetos/orientacao-objeto/modulo.py
@@ -1,78 +1,47 @@
-class Filme:
+class Programa:
+
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def ano(self):
+        return self._ano
+
+    @ano.setter
+    def ano(self, novo_ano):
+        self._ano = novo_ano
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def like(self):
+        self._likes += 1
+
+
+class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__duracao = duracao
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def ano(self):
-        return self.__ano
-
-    @ano.setter
-    def ano(self, novo_ano):
-        self.__ano = novo_ano
-
-    @property
-    def duracao(self):
-        return self.__duracao
-
-    @duracao.setter
-    def duracao(self, nova_duracao):
-        self.__duracao = nova_duracao
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def like(self):
-        self.__likes += 1
+        self._nome = nome.title()
+        self._ano = ano
+        self._duracao = duracao
+        self._likes = 0
 
 
-class Serie:
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.__ano = ano
-        self.__temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def ano(self):
-        return self.__ano
-
-    @ano.setter
-    def ano(self, novo_ano):
-        self.__ano = novo_ano
-
-    @property
-    def temporadas(self):
-        return self.__temporadas
-
-    @temporadas.setter
-    def temporadas(self, nova_temporada):
-        self.__temporadas = nova_temporada
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def like(self):
-        self.__likes += 1
+        self._nome = nome.title()
+        self._ano = ano
+        self._temporadas = temporadas
+        self._likes = 0
