d046bf07e9b4b20a5415c724c90b6cc62ab9307d
WybsonSantana <wybson@hotmail.com>
2024-06-15 18:27:25 -0300

    fpy05 - aula 4: Quantificadores e intervalos

--- fpy05-string-em-python/extrator-url/extrator_cep.py
+++ fpy05-string-em-python/extrator-url/extrator_cep.py
@@ -2,7 +2,7 @@ import re
 
 endereco = 'Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120'
 
-padrao_de_consulta = re.compile('[0-9]{5}-?[0-9]{3}')
+padrao_de_consulta = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
 busca = padrao_de_consulta.search(endereco)
 
 if busca:
