ee8df85635ea85df43051860dee945d3401c7561
WybsonSantana <wybson@hotmail.com>
2024-06-15 16:28:53 -0300

    fpy05 - aula 2: URL com múltiplos parâmetros

--- fpy05-string-em-python/extrator-url/main.py
+++ fpy05-string-em-python/extrator-url/main.py
@@ -9,8 +9,10 @@ print(url_base)
 url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
 
-parametro_busca = 'quantidade'
+parametro_busca = 'moedaOrigem'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
+valor = url_parametros[indice_valor:] if indice_e_comercial == -1 else url_parametros[indice_valor:indice_e_comercial]
+
 print(valor)
