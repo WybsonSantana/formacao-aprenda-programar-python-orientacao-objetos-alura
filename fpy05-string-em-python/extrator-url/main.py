3cbe340d92edd0cdb2e920d3a07209a2ed0198e8
WybsonSantana <wybson@hotmail.com>
2024-06-15 15:28:26 -0300

    fpy05 - aula 1: Fatiamento de strings

--- fpy05-string-em-python/extrator-url/main.py
+++ fpy05-string-em-python/extrator-url/main.py
@@ -1,2 +1,8 @@
 url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
 print(url)
+
+url_base = url[0:27]
+print(url_base)
+
+url_parametros = url[28:78]
+print(url_parametros)
