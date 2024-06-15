364d5647261ed826ee5e11fc99b5172708a24b23
WybsonSantana <wybson@hotmail.com>
2024-06-15 15:37:59 -0300

    fpy05 - aula 2: O método find()

--- fpy05-string-em-python/extrator-url/main.py
+++ fpy05-string-em-python/extrator-url/main.py
@@ -1,8 +1,10 @@
 url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
 print(url)
 
-url_base = url[0:27]
+indice_interrogacao = url.find('?')
+
+url_base = url[:indice_interrogacao]
 print(url_base)
 
-url_parametros = url[28:78]
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
