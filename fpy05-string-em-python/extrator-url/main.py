d2d44dd89ba23d1e1a6a4461c41d108efc2463cf
WybsonSantana <wybson@hotmail.com>
2024-06-15 16:00:36 -0300

    fpy05 - aula 2: O método len()

--- fpy05-string-em-python/extrator-url/main.py
+++ fpy05-string-em-python/extrator-url/main.py
@@ -8,3 +8,9 @@ print(url_base)
 
 url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
