6c7edffaf765b30a347c76ef58709e26b27dae4a
WybsonSantana <wybson@hotmail.com>
2024-06-16 13:15:13 -0300

    fpy06 - aula 1: Introdução as coleções e lista

---
+++ fpy06-collections-listas-tuplas/collections/main.py
@@ -0,0 +1,14 @@
+idades = [39, 30, 27, 18]
+
+idades.append(15)
+
+for idade in idades:
+    print(idade)
+
+idades.remove(30)
+idades.remove(27)
+idades.append(27)
+print(idades)
+
+idades.clear()
+print(idades)
