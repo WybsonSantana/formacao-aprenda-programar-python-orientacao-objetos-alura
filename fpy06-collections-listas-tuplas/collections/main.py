5fc064644f2544474ed36a00bcef23559ec1f3cb
WybsonSantana <wybson@hotmail.com>
2024-06-16 13:39:05 -0300

    fpy06 - aula 1: Mais operações em listas e list comprehension

--- fpy06-collections-listas-tuplas/collections/main.py
+++ fpy06-collections-listas-tuplas/collections/main.py
@@ -10,5 +10,21 @@ idades.remove(27)
 idades.append(27)
 print(idades)
 
-idades.clear()
+# idades.clear()
+# print(idades)
+
+if 15 in idades:
+    idades.remove(15)
+    print(idades)
+
+idades.insert(0, 20)
 print(idades)
+
+idades.extend([19, 28])
+print(idades)
+
+idades_no_ano_que_vem = [(idade + 1) for idade in idades]
+print(idades_no_ano_que_vem)
+
+idades_maiores_que_21 = [idade for idade in idades if idade > 21]
+print(idades_maiores_que_21)
