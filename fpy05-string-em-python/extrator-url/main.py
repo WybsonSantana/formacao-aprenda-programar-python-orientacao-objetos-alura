e9b4af8d55cedd1023523c4a88f717643acce08f
WybsonSantana <wybson@hotmail.com>
2024-06-16 01:30:45 -0300

    fpy05 - aula 5: Desafio: Conversão de moedas - dólar e real

--- fpy05-string-em-python/extrator-url/main.py
+++ fpy05-string-em-python/extrator-url/main.py
@@ -1,15 +1,42 @@
 from extrator_url import ExtratorURL
 
-url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
-#url = None
+MOEDAS = {
+    'real': {'dolar': lambda valor, cambio: valor / cambio},
+    'dolar': {'real': lambda valor, cambio: valor * cambio}
+}
 
-extrator_url = ExtratorURL(url)
-extrator_url_2 = ExtratorURL(url)
+SIMBOLOS_MOEDAS = {
+    'real': 'R$',
+    'dolar': 'US$'
+}
 
-print(extrator_url)
-print(f'Tamanho da URL: {len(extrator_url)} caractéres')
 
-valor_quantidade = extrator_url.get_valor_parametro('quantidade')
-print(valor_quantidade)
+def obter_parametros_de_moeda(url):
+    moeda_origem = url.get_valor_parametro('moedaOrigem').lower()
+    moeda_destino = url.get_valor_parametro('moedaDestino').lower()
+    valor_quantidade = float(url.get_valor_parametro('quantidade'))
+    return moeda_origem, moeda_destino, valor_quantidade
 
-print(extrator_url == extrator_url_2)
+
+def converter_moeda(url, cambio):
+    moeda_origem, moeda_destino, valor_quantidade = obter_parametros_de_moeda(url)
+
+    try:
+        valor_convertido = MOEDAS[moeda_origem][moeda_destino](valor_quantidade, cambio)
+        simbolo_moeda = SIMBOLOS_MOEDAS[moeda_destino]
+        return simbolo_moeda, valor_convertido
+    except KeyError:
+        raise ValueError('Não foi possível realizar a conversão!')
+
+
+def main():
+    url = 'https://bytebank.com/cambio?moedaOrigem=dolar&moedaDestino=real&quantidade=100'
+    extrator_url = ExtratorURL(url)
+
+    valor_dolar = 5.5
+
+    simbolo_moeda, valor_convertido = converter_moeda(extrator_url, valor_dolar)
+    print(f'Valor convertido: {simbolo_moeda} {valor_convertido:.2f}')
+
+
+main()
