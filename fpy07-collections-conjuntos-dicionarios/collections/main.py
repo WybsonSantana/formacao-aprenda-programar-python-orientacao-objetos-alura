750d0a5ac637e9111288ab1ca6ee20022f580b16
WybsonSantana <wybson@hotmail.com>
2024-06-22 11:43:12 -0300

    fpy07 - aula 2: Outro tipo de conjunto e conjuntos de outros tipos

--- fpy07-collections-conjuntos-dicionarios/collections/main.py
+++ fpy07-collections-conjuntos-dicionarios/collections/main.py
@@ -40,3 +40,26 @@ print(15 in fez_ds_mas_nao_fez_ml)
 print(23 in fez_ds_mas_nao_fez_ml)
 
 print(usuarios_data_science ^ usuarios_machine_learning)
+
+usuarios = {1, 5, 76, 34, 52, 13, 17}
+print(len(usuarios))
+
+usuarios.add(13)
+print(len(usuarios))
+
+usuarios.add(765)
+print(len(usuarios))
+
+print(usuarios)
+
+usuarios = frozenset(usuarios)
+print(usuarios)
+
+print(type(usuarios))
+
+# usuarios.add(134)
+
+meu_texto = 'Bem vindo meu nome é Fulano eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
+print(meu_texto.split())
+
+print(set(meu_texto.split()))
