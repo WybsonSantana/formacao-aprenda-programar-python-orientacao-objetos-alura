3fdaf9ccbd98ed3f2ea6e05c02a5c985b1f531bf
WybsonSantana <wybson@hotmail.com>
2024-06-22 12:20:13 -0300

    fpy07 - aula 4: Default dict

--- fpy07-collections-conjuntos-dicionarios/collections/main.py
+++ fpy07-collections-conjuntos-dicionarios/collections/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 usuarios_data_science = [15, 23, 43, 56]
 usuarios_machine_learning = [13, 23, 56, 42]
 
@@ -131,3 +133,30 @@ for chave, valor in aparicoes.items():
     print(chave, '=', valor)
 
 print(["palavra {}".format(chave) for chave in aparicoes.keys()])
+
+meu_texto = 'bem vindo meu nome é Fulano eu gosto muito de nomes e tenho o meu cachorro e gosto muito de cachorro'
+meu_texto = meu_texto.lower()
+
+aparicoes = {}
+
+for palavra in meu_texto.split():
+    ate_agora = aparicoes.get(palavra, 0)
+    aparicoes[palavra] = ate_agora + 1
+
+print(aparicoes)
+
+aparicoes = defaultdict(int)
+
+for palavra in meu_texto.split():
+    ate_agora = aparicoes[palavra]
+    aparicoes[palavra] = ate_agora + 1
+
+print(aparicoes)
+
+int()
+
+dicionario = defaultdict(int)
+dicionario['Fulano']
+
+dicionario['Fulano'] = 15
+print(dicionario['Fulano'])
