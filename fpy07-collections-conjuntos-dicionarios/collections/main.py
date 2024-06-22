948d99e101717c6f39a461cd08a6b27c6d68ac31
WybsonSantana <wybson@hotmail.com>
2024-06-22 12:02:08 -0300

    fpy07 - aula 3: Mais operações de dicionários

--- fpy07-collections-conjuntos-dicionarios/collections/main.py
+++ fpy07-collections-conjuntos-dicionarios/collections/main.py
@@ -92,3 +92,42 @@ aparicoes = {
     'nome': 2,
     'vindo': 1
 }
+
+aparicoes['Beltrano'] = 1
+
+print(aparicoes)
+
+aparicoes['Beltrano'] = 2
+
+print(aparicoes)
+
+del aparicoes['Beltrano']
+
+print(aparicoes)
+
+print('cachorro' in aparicoes)
+
+print('Beltrano' in aparicoes)
+
+for elemento in aparicoes:
+    print(elemento)
+
+for elemento in aparicoes.keys():
+    print(elemento)
+
+for elemento in aparicoes.values():
+    print(elemento)
+
+print(1 in aparicoes.values())
+
+for elemento in aparicoes.keys():
+    valor = aparicoes[elemento]
+    print(elemento, valor)
+
+for elemento in aparicoes.items():
+    print(elemento)
+
+for chave, valor in aparicoes.items():
+    print(chave, '=', valor)
+
+print(["palavra {}".format(chave) for chave in aparicoes.keys()])
