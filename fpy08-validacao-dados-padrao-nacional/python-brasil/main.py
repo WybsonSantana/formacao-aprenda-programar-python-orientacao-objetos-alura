fac5f4ea69ff0917dece93958a91384d56b30d1e
WybsonSantana <wybson@hotmail.com>
2024-06-29 01:43:13 -0300

    fpy08 - aula 3: Definindo padrão para Telefones

--- fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
+++ fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
@@ -1,13 +1,5 @@
-import re
+from telefones_br import TelefonesBr
 
-padrao = '[0-9][a-z]{2}[0-9]'
-texto = '123 1ac2 1cc aa1'
-resposta = re.search(padrao, texto)
+telefone = '552126481234'
 
-print(resposta.group())
-
-padrao = '\\w{5,50}@[a-z]{3,10}.com.br'
-texto = 'aaabbbcc rodrigo123@gmail.com.br'
-resposta = re.search(padrao, texto)
-
-print(resposta.group())
+telefone_objeto = TelefonesBr(telefone)
