2bd17cb390a1f6de91cb6f802795b52a10aa9f85
WybsonSantana <wybson@hotmail.com>
2024-06-29 01:59:32 -0300

    fpy08 - aula 3: Criando Máscara para o numero de telefone

--- fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
+++ fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
@@ -3,3 +3,4 @@ from telefones_br import TelefonesBr
 telefone = '552126481234'
 
 telefone_objeto = TelefonesBr(telefone)
+print(telefone_objeto)
--- fpy08-validacao-dados-padrao-nacional/python-brasil/telefones_br.py
+++ fpy08-validacao-dados-padrao-nacional/python-brasil/telefones_br.py
@@ -2,17 +2,30 @@ import re
 
 
 class TelefonesBr:
-
     def __init__(self, telefone):
         if self.validar_telefone(telefone):
             self.numero = telefone
         else:
             raise ValueError('Número incorreto!')
 
+    def __str__(self):
+        return self.formatar_numero()
+
     def validar_telefone(self, telefone):
-        padrao = '[0-9]{2,3}[0-9]{2}[0-9]{4,5}[0-9]{4}'
+        padrao = '([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})'
         resposta = re.findall(padrao, telefone)
         if resposta:
             return True
         else:
             return False
+
+    def formatar_numero(self):
+        padrao = '([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})'
+        resposta = re.search(padrao, self.numero)
+        numero_formatado = '+{} ({}) {}-{}'.format(
+            resposta.group(1),
+            resposta.group(2),
+            resposta.group(3),
+            resposta.group(4)
+        )
+        return numero_formatado
