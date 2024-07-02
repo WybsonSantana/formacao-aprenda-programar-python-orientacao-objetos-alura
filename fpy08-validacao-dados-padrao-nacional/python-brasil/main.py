a47a7ce64d9b3a613425f45fce7cd18c524329ef
WybsonSantana <wybson@hotmail.com>
2024-07-02 12:27:40 -0300

    fpy08 - aula 5: Acessando APIs com Python

--- fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
+++ fpy08-validacao-dados-padrao-nacional/python-brasil/main.py
@@ -1,6 +1,13 @@
+import requests
+
 from acesso_cep import BuscaEndereco
 
 cep = 25870145
 objeto_cep = BuscaEndereco(cep)
-
 print(objeto_cep)
+
+request = requests.get('https://viacep.com.br/ws/01001000/json/')
+print(request.text)
+
+acesso = objeto_cep.acessar_via_cep()
+print(acesso)
