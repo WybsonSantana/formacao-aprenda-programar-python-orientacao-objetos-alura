0f7ee7e347327bd68d66aa3c1516136ef75ffb1b
WybsonSantana <wybson@hotmail.com>
2024-07-02 21:09:16 -0300

    fpy09 - aula 4: Um método com Exception

--- fpy09-explorando-testes-unitarios/bytebank/main.py
+++ fpy09-explorando-testes-unitarios/bytebank/main.py
@@ -1,8 +1,4 @@
 from bytebank import Funcionario
 
-
-def teste_idade():
-    funcionario_teste = Funcionario('Teste', '17/03/2000', 1111)
-    print(f'Teste = {funcionario_teste.idade()}')
-
-teste_idade()
+fulano = Funcionario('Fulano de Tal', '01/01/1991', 15000)
+print(fulano.calcular_bonus())
--- fpy09-explorando-testes-unitarios/bytebank/tests/test_bytebank.py
+++ fpy09-explorando-testes-unitarios/bytebank/tests/test_bytebank.py
@@ -31,3 +31,12 @@ class TestClass:
         resultado = funcionario_teste.salario
 
         assert resultado == esperado
+
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000
+        esperado = 100
+
+        funcionario_teste = Funcionario('teste', '11/11/2000', entrada)
+        resultado = funcionario_teste.calcular_bonus()
+
+        assert resultado == esperado
